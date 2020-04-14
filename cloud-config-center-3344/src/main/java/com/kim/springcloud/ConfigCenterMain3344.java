23cb45dd6f77ef68bd295ba769260401948aa06a
yjsdev <yjs_good_job@163.com>
2020-04-14 16:56:50 +0800

    update

---
+++ cloud-config-center-3344/src/main/java/com/kim/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,18 @@
+package com.kim.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @auther kim
+ * @create 2020-04-14 16:55
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344
+{
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class, args);
+    }
+}
